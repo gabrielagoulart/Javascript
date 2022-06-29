e459063168aa12004c73a6bab4677f40169bcd65
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-29 19:01:53 -0300

    Sortear número

---
+++ exercícios/exercicio012/sorteio.js
@@ -0,0 +1,16 @@
+function gerar() {
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random() //gera um valor entre 0 e 1
+    let num = min + Math.trunc(dif * aleatorio)
+
+    let res = document.querySelector('section#result')
+    res.innerHTML += `<p>Acabei de pensar no número <mark>${num}</mark>!</p>`
+}
+
+
+function limpar() {
+    let res = document.querySelector('section#result')
+    res.innerHTML = null
+}
